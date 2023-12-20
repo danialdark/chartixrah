89f95b97e0d1bcdb7088d3def6dff53208995dd1
Danial <danialdark12098@gmail.com>
2023-12-20 14:47:34 +0330

    changing it

--- symbol.js
+++ symbol.js
@@ -2,27 +2,43 @@ const db = require('./db'); // Adjust the path as needed
 const axios = require('axios');
 
 
+async function checkStatus(dataTicker) {
+    const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+
+    const response = await axios.get(`https://www.nahayatnegar.com/tv/chart/history?symbol=${dataTicker}0&resolution=1D&from=0&to=${currentTimestampInSeconds}&countback=2`);
+    if (response.data.t == null) {
+        return 0;
+    } else {
+        if (response.data.t.reverse()[0] > currentTimestampInSeconds - 259200) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+}
+
 const getData = async () => {
     const response = await axios.get(`http://87.107.190.134/brsModern/out/rsSymOutActive.php`);
     var serverNumber = 1;
     var serverCounter = 1;
 
-
     for (const data of response.data) {
 
-        if (serverCounter % 300 == 0) {
+        if (serverCounter % 400 == 0) {
             serverNumber++;
         }
 
-        insertOrUpdateSymbolToDatabase(data.ticker.toUpperCase(), data, serverNumber)
-        serverCounter++
 
+        const status = await checkStatus(data.bourse_ticker.toUpperCase())
+        insertOrUpdateSymbolToDatabase(data.ticker.toUpperCase(), data, serverNumber, status)
+        serverCounter++
     }
 }
 
 
 
-async function insertOrUpdateSymbolToDatabase(symbol, symbolData, serverNumber) {
+async function insertOrUpdateSymbolToDatabase(symbol, symbolData, serverNumber, status) {
     const {
         title,
         alias,
@@ -42,6 +58,7 @@ async function insertOrUpdateSymbolToDatabase(symbol, symbolData, serverNumber) 
               username = excluded.username,
               alias = excluded.alias,
               title = excluded.title,
+              status = excluded.status,
               description = excluded.description,
               server = excluded.server,
               exchange = excluded.exchange;`,
@@ -50,7 +67,7 @@ async function insertOrUpdateSymbolToDatabase(symbol, symbolData, serverNumber) 
                 bourse_ticker != null ? bourse_ticker : "ندارد",
                 title != null ? title : "ندارد",
                 alias != null ? alias : "ندارد",
-                1,
+                status,
                 description != null ? description : "ندارد",
                 exchange != null ? exchange : "ندارد",
                 1,
@@ -58,7 +75,7 @@ async function insertOrUpdateSymbolToDatabase(symbol, symbolData, serverNumber) 
                 serverNumber
             ]
         );
-        console.log(`Inserted/Updated symbol: ${symbol} on server:${serverNumber}`);
+        console.log(`Inserted/Updated symbol: ${symbol} on server:${serverNumber} that is ${status == 0 ? 'inactive' : "active"}`);
     } catch (error) {
         console.error(`Error saving symbol ${symbol} to PostgreSQL:`, error);
     }
